5b36385439e2510652f733e7866422e58c17f636
vaibhav gehani <vgehani90@gmail.com>
2020-12-11 18:30:07 +0530

    camera plugin

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue'
+import Page from '../views/Page.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -11,6 +12,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/page',
+    name: 'Page',
+    component: Page
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,67 +2,94 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title >Camera Application</ion-title>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <div class="container">
+      <ion-button @click="ClickImage()">Click Image</ion-button>
+      </div>
+      <div class="container">
+      <ion-img v-if="render" class="img" :src="imageUrl"/>
+      </div>
+      <div class="container">
+      <ion-button v-if="render" @click="Save()">Save</ion-button>
+      <ion-button v-if="render" @click="Clear()">Clear</ion-button>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg, IonButton, IonRouter, } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+import { Plugins, CameraResultType, CameraSource, FilesystemDirectory } from '@capacitor/core';
+
 
 export default defineComponent({
+  data(){ return  {imageUrl : '', render: false}},
   name: 'Home',
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonButton,
+    IonImg,
+  },
+  methods: {
+    async ClickImage() {
+      const { Camera } = Plugins;
+      await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Uri
+        }).then((image) => {
+          this.render = true;
+          this.imageUrl = String(image.webPath)
+      });
+    },
+    convertBlobToBase64 (blob: Blob) {
+      return new Promise((resolve, reject) =>{
+        const reader = new FileReader;
+        reader.onerror = reject;
+        reader.onload = () => {
+          resolve(reader.result);
+        };
+        reader.readAsDataURL(blob);
+      });
+    },
+    async Save() {
+      const {Filesystem} = Plugins;
+        const response = await fetch(this.imageUrl);
+        const blob = await response.blob();
+        const base64Data = await this.convertBlobToBase64(blob) as string;
+        console.log('url', base64Data);
+        const savedFile = await Filesystem.writeFile({
+            path: new Date().getTime() + '.jpeg',
+            data: base64Data,
+            directory: FilesystemDirectory.Data
+      });
+    }
   }
 });
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+.container {
+  flex: 1;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-content: center;
+  margin-top: 12px;
+}
+.img {
+  height: 300px;
+  width: 300px;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
